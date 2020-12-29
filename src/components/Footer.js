19278cfbdca2c29244e042d64293c31cab9b247b
temuujin <temuujin.mt@outlook.com>
2020-12-30 02:11:04 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
         <div className="inner">
             <ul className="icons">
                 <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                <li><a href="https://www.facebook.com/mariner.mind.99/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                 <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
