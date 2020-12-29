e540457d70b8d629822948dc299264afb30ca1b0
temuujin <temuujin.mt@outlook.com>
2020-12-30 01:21:30 +0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Сайн байцгаана уу? Миний хөгжүүлж буй шинэ вэб сайтад тавтай морил</h1>
             </header>
             <div className="content">
                 <p>A responsive site template designed by HTML5 UP<br />
@@ -18,3 +18,4 @@ const Banner = (props) => (
 )
 
 export default Banner
+
