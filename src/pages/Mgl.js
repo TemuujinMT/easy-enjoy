6b4e0fd25dd4dc076b186624e707be47913931c7
temuujin <temuujin.mt@outlook.com>
2021-01-01 14:57:00 +0800

    Create Mgl.js

---
+++ src/pages/Mgl.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import BannerLanding from '../components/BannerLanding'
+
+import pic08 from '../assets/images/pic08.jpg'
+import pic09 from '../assets/images/pic09.jpg'
+import pic10 from '../assets/images/pic10.jpg'
+
+const Mgl = (props) => (
+    <Layout>
+        <Helmet>
+            <title>mgl - Forty by HTML5 UP</title>
+            <meta name="description" content="mgl Page" />
+        </Helmet>
+
+        <BannerLanding />
+
+
+
+export default Elements
